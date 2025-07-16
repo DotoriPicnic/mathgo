5eea4c519dc5d08dbc5c4f6917586074ff828a19
DotoriPicnic <ysgille@naver.com>
2025-07-16 10:04:20 +0900

    Fix: remove base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/mathgo/',  // 여기 리포지토리 이름 넣기
   plugins: [react()],
+})
+*/
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  // ✅ Vercel에서는 base 설정 절대 넣지 마세요!
 })
